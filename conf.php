031a5912477641327904c4a704f7e74497686f56
ds18154 <73101184+ds18154@users.noreply.github.com>
2022-01-13 17:59:30 +0200

    Delete conf.php

--- conf.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define( 'DB_PWD', '20diabetic21broccoli' );   
-   define( 'USERNAME', 'diabeticbroccoli' );
-   define( 'DATABASE', 'diabeticbroccoli' );   
-   define( 'HOST', 'nireas.it.teithe.gr' );   
-?>
-
