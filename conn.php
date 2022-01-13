323e667d44cb04916a9bf09b4011d2891f7ba432
ds18154 <73101184+ds18154@users.noreply.github.com>
2022-01-13 17:59:40 +0200

    Delete conn.php

--- conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $conn=mysqli_connect("localhost", "diabeticbroccoli", "20diabetic21broccoli", "diabeticbroccoli");
- 
-    if(!$conn){
-        die("Error: Failed to connect to database!");
-    }
-?>
