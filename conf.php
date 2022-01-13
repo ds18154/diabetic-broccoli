096ac1f4550fb7c77746704ec3a0a800749e3110
ds18154 <73101184+ds18154@users.noreply.github.com>
2022-01-13 17:58:15 +0200

    Add files via upload

---
+++ conf.php
@@ -0,0 +1,7 @@
+<?php
+   define( 'DB_PWD', '20diabetic21broccoli' );   
+   define( 'USERNAME', 'diabeticbroccoli' );
+   define( 'DATABASE', 'diabeticbroccoli' );   
+   define( 'HOST', 'nireas.it.teithe.gr' );   
+?>
+
